d4918fee9fdec59c140883a470c1514c8b0ce946
Presswood79 <1presswoogav@hdsb.ca>
2024-09-05 18:57:19 +0000

    I add to lines of code

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,6 @@
 // Log "Hello World" to the console.
 // Log four additional messages of your choosing. 
 // Comment each message appropriately. 
-console.log("Testing!");
+console.log("Testing!"); 
+console.log("I am cool!"); 
+console.log("I am amazing!");
